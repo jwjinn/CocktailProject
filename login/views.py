55a02ab40e97d482516620a873bcce8c468ceca6
jwjinn <jwjinn@gmail.com>
2023-01-03 14:39:16 +0900

    "모바일 테스트용 push"

--- login/views.py
+++ login/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+
+
+
+def register(request):
+
+    if request.method == 'GET':
+        return render(request, 'register.html')
+
+    if request.method == 'POST':
+        print(request.POST['email'])
+        return redirect('index')
 
