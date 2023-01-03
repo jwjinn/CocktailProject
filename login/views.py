672dea987a62f6b3faead546244b13fb9d6382e7
jwjinn <jwjinn@gmail.com>
2023-01-03 15:03:39 +0900

    "모바일 화면 테스트용 3"

--- login/views.py
+++ login/views.py
@@ -9,6 +9,11 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
 
+    if request.method == 'POST':
+        print(request.POST['email'])
+        print(request.POST['password'])
+        return redirect('index')
+
 
 
 
@@ -19,5 +24,10 @@ def register(request):
 
     if request.method == 'POST':
         print(request.POST['email'])
+        print(request.POST['password'])
+        print(request.POST['name'])
+        print(request.POST['gender'])
+        print(request.POST['age'])
+        print(request.POST['phone_number'])
         return redirect('index')
 
